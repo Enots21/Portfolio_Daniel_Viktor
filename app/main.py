b4c9d64bc040755e0145cd6bf147cec9e15ed2f1
ViktorVersh <vershininzaig@mail.ru>
2025-02-19 21:40:22 +0800

    1

--- app/main.py
+++ app/main.py
@@ -5,17 +5,16 @@ from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 
-
-app = FastAPI() # create an instance of FastAPI
+app = FastAPI()  # create an instance of FastAPI
 # Указываем папку для статических файлов
 static_dir = os.path.join(os.path.dirname(__file__), "static")
 app.mount("/static", StaticFiles(directory=static_dir), name="static")
 
 # Указываем папку с шаблонами
-templates = Jinja2Templates(directory="app/templates")
+templates = Jinja2Templates(directory="templates")
 
 
-@app.get("/", response_class=HTMLResponse) # Начало
+@app.get("/", response_class=HTMLResponse)  # Начало
 async def read_root(request: Request):
     # Передаем запрос и имя шаблона в метод render
     return templates.TemplateResponse("index.html", {"request": request})
@@ -25,6 +24,8 @@ async def read_root(request: Request):
 async def read_Daniel(request: Request):
     return templates.TemplateResponse("Daniel.html", {"request": request})
 
-@app.get("/viktor", response_class=HTMLResponse) # Портфолио Виктора
+
+@app.get("/viktor", response_class=HTMLResponse)
 async def read_Viktor(request: Request):
-    return templates.TemplateResponse("Viktor.html", {"request": request})
+    image_url = "/static/file/img/Viktor.jpg"  # Указываем путь к изображению
+    return templates.TemplateResponse("Viktor.html", {"request": request, "image_url": image_url})
